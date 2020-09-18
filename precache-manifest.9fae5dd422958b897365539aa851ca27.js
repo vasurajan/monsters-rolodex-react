f51aad51ae3e51e2df6ef0a4fa40474409d460dc
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-09-19 01:55:59 +0530

    Updates

---
+++ precache-manifest.9fae5dd422958b897365539aa851ca27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867bb0edbb46f9d5b45a5e0ef958f1c9",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "a0ef41aa4ffa27f12dc2",
+    "url": "/monsters-rolodex-react/static/css/main.92d63577.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ef41aa4ffa27f12dc2",
+    "url": "/monsters-rolodex-react/static/js/main.434de241.chunk.js"
+  },
+  {
+    "revision": "ab8efdbffd13bd88af79",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.524445f2.js"
+  }
+]);
